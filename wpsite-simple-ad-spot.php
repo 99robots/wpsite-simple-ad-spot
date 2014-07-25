c6e78c1c8b1d9943ad677a7e06180aa4f40c6d29
Kyle <kjbenk@gmail.com>
2014-07-25 17:45:22 -0400

    Minor updates

--- wpsite-simple-ad-spot.php
+++ wpsite-simple-ad-spot.php
@@ -114,7 +114,7 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 		if (isset($instance['ad_sense']) && $instance['ad_sense'] != '') {
 			echo $instance['ad_sense'];
 		} else if (isset($instance['image']) && isset($instance['link']) && $instance['image'] != '' && $instance['link'] != '') {
-			echo '<div class="wps-sas-container"><a class="wps-sas-link" href="' . $instance['link'] . '" target="_blank"><img class="wps-sas-image" src="' . $instance['image'] . '"/></a></div>';
+			echo '<div class="wps-sas-container" style="max-width:100%;overflow:hidden;"><a class="wps-sas-link" href="' . $instance['link'] . '" target="_blank"><img class="wps-sas-image" src="' . $instance['image'] . '"/></a></div>';
 		}
 
 		echo $args['after_widget'];
@@ -169,13 +169,13 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Image URL:', self::$text_domain); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" type="text" value="<?php echo esc_url( $image ); ?>"><br/>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" type="text" value="<?php echo esc_url( $image ); ?>" placeholder="http://example.com/image.png"><br/>
 			<em><?php _e('URL to the image you want to display', self::$text_domain); ?></em>
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Link:', self::$text_domain); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_url( $link ); ?>"><br/>
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Destination URL:', self::$text_domain); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_url( $link ); ?>" placeholder="http://example.com"><br/>
 			<em><?php _e('Where you want the image to link', self::$text_domain); ?></em>
 		</p>
 
@@ -198,7 +198,7 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
-		$instance['ad_sense'] = ( ! empty( $new_instance['ad_sense'] ) ) ? sanitize_text_field( $new_instance['ad_sense'] ) : '';
+		$instance['ad_sense'] = ( ! empty( $new_instance['ad_sense'] ) ) ? $new_instance['ad_sense'] : '';
 		$instance['image'] = ( ! empty( $new_instance['image'] ) ) ? sanitize_text_field( $new_instance['image'] ) : '';
 		$instance['link'] = ( ! empty( $new_instance['link'] ) ) ? sanitize_text_field( $new_instance['link'] ) : '';
 
@@ -239,19 +239,6 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 		$settings_link = '<a href="widgets.php">Widget</a>';
 		array_unshift($links, $settings_link);
 		return $links;
-	}
-
-	/**
-	 * Hooks to 'admin_print_scripts-$page'
-	 *
-	 * @since 1.0.0
-	 */
-	static function include_admin_scripts() {
-
-		/* CSS */
-
-		wp_register_style(self::$prefix . 'style_css', WPSITE_SIMPLE_AD_SPOT_PLUGIN_URL . '/css/style.css');
-		wp_enqueue_style(self::$prefix . 'style_css');
 	}
 }
 
