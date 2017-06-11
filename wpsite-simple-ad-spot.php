2dfb6763a75b3ce74c3afb5e1b3579ec7cfb0d90
Shakeeb Ahmed <shakeeb.a@mythemeshop.com>
2017-06-11 21:22:18 +0500

    compatible with wp 4.8

--- wpsite-simple-ad-spot.php
+++ wpsite-simple-ad-spot.php
@@ -1,97 +1,42 @@
 <?php
-/*
-Plugin Name: Simple Ad Spot
-plugin URI: https://www.99robots.com
-Description: The 99 Robots Simple Ad Spot plugin is the easiest way to place an advertisement in your WordPress site.
-version: 1.1.1
-Author: 99 Robots
-Author URI: https://www.99robots.com
-License: GPL2
-*/
-
 /**
- * Global Definitions
+ * Plugin Name: Simple Ad Spot
+ * Plugin URI: https://www.99robots.com
+ * Description: The 99 Robots Simple Ad Spot plugin is the easiest way to place an advertisement in your WordPress site.
+ * Version: 1.1.1
+ * Author: 99 Robots
+ * Author URI: https://www.99robots.com
+ * License: GPL2
  */
-
-/* Plugin Name */
-
-if (!defined('WPSITE_SIMPLE_AD_SPOT_PLUGIN_NAME'))
-    define('WPSITE_SIMPLE_AD_SPOT_PLUGIN_NAME', trim(dirname(plugin_basename(__FILE__)), '/'));
-
-/* Plugin directory */
-
-if (!defined('WPSITE_SIMPLE_AD_SPOT_PLUGIN_DIR'))
-    define('WPSITE_SIMPLE_AD_SPOT_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . WPSITE_SIMPLE_AD_SPOT_PLUGIN_NAME);
-
-/* Plugin url */
-
-if (!defined('WPSITE_SIMPLE_AD_SPOT_PLUGIN_URL'))
-    define('WPSITE_SIMPLE_AD_SPOT_PLUGIN_URL', WP_PLUGIN_URL . '/' . WPSITE_SIMPLE_AD_SPOT_PLUGIN_NAME);
-
-/* Plugin verison */
-
-if (!defined('WPSITE_SIMPLE_AD_SPOT_VERSION_NUM'))
-    define('WPSITE_SIMPLE_AD_SPOT_VERSION_NUM', '1.1.1');
-
-
-/**
- * Activatation / Deactivation
- */
-
-register_activation_hook( __FILE__, array('WPsiteSimpleAdSpot', 'register_activation'));
 
 /**
  * Hooks / Filter
  */
+add_action( 'widgets_init', function() {
+	register_widget( 'WPsite_Simple_Ad_Spot' );
+} );
 
-add_action('widgets_init',
-     create_function('', 'return register_widget("WPsiteSimpleAdSpot");')
-);
-
-add_action('init', array('WPsiteSimpleAdSpot', 'load_textdomain'));
-
-$plugin = plugin_basename(__FILE__);
-add_filter("plugin_action_links_$plugin", array('WPsiteSimpleAdSpot', 'plugin_links'));
+$plugin = plugin_basename( __FILE__ );
+add_action( 'init', array( 'WPsite_Simple_Ad_Spot', 'load_textdomain' ) );
+add_filter( "plugin_action_links_$plugin", array( 'WPsite_Simple_Ad_Spot', 'plugin_links' ) );
 
 /**
- *  WPsiteSimpleAdSpot main class
+ *  WPsite_Simple_Ad_Spot main class
  *
  * @since 1.0.0
  * @using Wordpress 3.8
  */
-
-class WPsiteSimpleAdSpot extends WP_Widget {
-
-	/**
-	 * text_domain
-	 *
-	 * (default value: 'wpsite_simple_ad_spot')
-	 *
-	 * @var string
-	 * @access private
-	 * @static
-	 */
-	private static $text_domain = 'wpsite_simple_ad_spot';
-
-	/**
-	 * prefix
-	 *
-	 * (default value: 'wpsite_simple_ad_spot_')
-	 *
-	 * @var string
-	 * @access private
-	 * @static
-	 */
-	private static $prefix = 'wpsite_simple_ad_spot_';
+class WPsite_Simple_Ad_Spot extends WP_Widget {
 
 	/**
 	 * Register widget with WordPress.
 	 */
 	function __construct() {
+
 		parent::__construct(
-			'wpsite-simple-ad-spot', // Base ID
-			__('99 Robots Simple Ad Spot', self::$text_domain), // Name
-			array( 'description' => __( 'Display an Ad', self::$text_domain), ) // Args
+			'wpsite-simple-ad-spot',
+			esc_html__( '99 Robots Simple Ad Spot', 'wpsite_simple_ad_spot' ),
+			array( 'description' => esc_html__( 'Display an Ad', 'wpsite_simple_ad_spot' ) )
 		);
 	}
 
@@ -104,16 +49,18 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
+
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
 		echo $args['before_widget'];
+
 		if ( ! empty( $title ) ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
 
-		if (isset($instance['ad_sense']) && $instance['ad_sense'] != '') {
+		if ( isset( $instance['ad_sense'] ) && '' !== $instance['ad_sense'] ) {
 			echo $instance['ad_sense'];
-		} else if (isset($instance['image']) && isset($instance['link']) && $instance['image'] != '' && $instance['link'] != '') {
+		} elseif ( isset( $instance['image'] ) && isset( $instance['link'] ) && '' !== $instance['image'] && '' !== $instance['link'] ) {
 			echo '<div class="wps-sas-container" style="max-width:100%;overflow:hidden;"><a class="wps-sas-link" href="' . $instance['link'] . '" target="_blank"><img class="wps-sas-image" src="' . $instance['image'] . '"/></a></div>';
 		}
 
@@ -128,59 +75,37 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-		}
-		else {
-			$title = __( 'Simple Ad Spot', self::$text_domain);
-		}
 
-		if ( isset( $instance[ 'ad_sense' ] ) ) {
-			$ad_sense = $instance[ 'ad_sense' ];
-		}
-		else {
-			$ad_sense = '';
-		}
-
-		if ( isset( $instance[ 'image' ] ) ) {
-			$image = $instance[ 'image' ];
-		}
-		else {
-			$image = '';
-		}
-
-		if ( isset( $instance[ 'link' ] ) ) {
-			$link = $instance[ 'link' ];
-		}
-		else {
-			$link = '';
-		}
+		$title = isset( $instance['title'] ) ? $instance['title'] : esc_html__( 'Simple Ad Spot', 'wpsite_simple_ad_spot' );
+		$ad_sense = isset( $instance['ad_sense'] ) ? $instance['ad_sense'] : '';
+		$image = isset( $instance['image'] ) ? $instance['image'] : '';
+		$link = isset( $instance['link'] ) ? $instance['link'] : '';
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', self::$text_domain); ?></label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'wpsite_simple_ad_spot' ) ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'ad_sense' ); ?>"><?php _e( 'Ad Sense Code:', self::$text_domain); ?></label>
+			<label for="<?php echo $this->get_field_id( 'ad_sense' ); ?>"><?php esc_html_e( 'Ad Sense Code:', 'wpsite_simple_ad_spot' ) ?></label>
 			<textarea cols="46" rows="10" id="<?php echo $this->get_field_id( 'ad_sense' ); ?>" name="<?php echo $this->get_field_name( 'ad_sense' ); ?>"><?php echo esc_attr( $ad_sense ); ?></textarea>
-			<em><?php _e('Leave this blank if you want to use the Image and Link instead.', self::$text_domain); ?></em>
+			<em><?php esc_html_e( 'Leave this blank if you want to use the Image and Link instead.', 'wpsite_simple_ad_spot' ) ?></em>
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Image URL:', self::$text_domain); ?></label>
+			<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php esc_html_e( 'Image URL:', 'wpsite_simple_ad_spot' ) ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" type="text" value="<?php echo esc_url( $image ); ?>" placeholder="http://example.com/image.png"><br/>
-			<em><?php _e('URL to the image you want to display', self::$text_domain); ?></em>
+			<em><?php esc_html_e( 'URL to the image you want to display', 'wpsite_simple_ad_spot' ) ?></em>
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Destination URL:', self::$text_domain); ?></label>
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php esc_html_e( 'Destination URL:', 'wpsite_simple_ad_spot' ) ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_url( $link ); ?>" placeholder="http://example.com"><br/>
-			<em><?php _e('Where you want the image to link', self::$text_domain); ?></em>
+			<em><?php esc_html_e( 'Where you want the image to link', 'wpsite_simple_ad_spot' ) ?></em>
 		</p>
 
 		<p>
-			<span><?php _e("This widget's css id is:", self::$text_domain); ?></span> <strong><?php echo $this->id; ?></strong>
+			<span><?php esc_html_e( 'This widget\'s css id is:', 'wpsite_simple_ad_spot' ) ?></span> <strong><?php echo $this->id; ?></strong>
 		</p>
 		<?php
 	}
@@ -211,23 +136,19 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 	 * @since 1.0.0
 	 */
 	static function load_textdomain() {
-		load_plugin_textdomain(self::$text_domain, false, WPSITE_SIMPLE_AD_SPOT_PLUGIN_DIR . '/languages');
-	}
 
-	/**
-	 * Hooks to 'register_activation_hook'
-	 *
-	 * @since 1.0.0
-	 */
-	static function register_activation() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'wpsite_simple_ad_spot' );
 
-		/* Check if multisite, if so then save as site option */
+		load_textdomain(
+			'wpsite_simple_ad_spot',
+			WP_LANG_DIR . '/wpsite-simple-ad-spot/wpsite-simple-ad-spot-' . $locale . '.mo'
+		);
 
-		if (function_exists('is_multisite') && is_multisite()) {
-			add_site_option(self::$prefix . 'version', WPSITE_SIMPLE_AD_SPOT_VERSION_NUM);
-		} else {
-			add_option(self::$prefix . 'version', WPSITE_SIMPLE_AD_SPOT_VERSION_NUM);
-		}
+		load_plugin_textdomain(
+			'wpsite_simple_ad_spot',
+			false,
+			untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/languages/'
+		);
 	}
 
 	/**
@@ -235,11 +156,11 @@ class WPsiteSimpleAdSpot extends WP_Widget {
 	 *
 	 * @since 1.0.0
 	 */
-	static function plugin_links($links) {
+	static function plugin_links( $links ) {
+
 		$settings_link = '<a href="widgets.php">Widget</a>';
-		array_unshift($links, $settings_link);
+		array_unshift( $links, $settings_link );
+
 		return $links;
 	}
 }
-
-?>
